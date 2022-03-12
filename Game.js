ff7365ad1e41e1da47f9a6d2b1ed45936a3897b1
bar <barharel77@gmail.com>
2022-03-12 12:06:13 +0200

    stam1

---
+++ Game.js
@@ -0,0 +1,18 @@
+
+
+function playRound(playerSelection, computerSelection) {
+  for (let i = 0; i < 5; i++) {
+    function computerPlay ()
+    let computerPlay = random (Rock, Paper, Scissors);
+  }
+  }
+
+  if computerPlay == "Rock" {
+    return "Tie!"
+  } else if computerPlay == "Papper" {
+    return "You Won!"
+  } else return "You Lose!"
+  
+  const playerSelection = "rock";
+  const computerSelection = computerPlay();
+  console.log(playRound(playerSelection, computerSelection)); 
